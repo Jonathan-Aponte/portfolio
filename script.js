f9b51fe115cc915430f780cf2c3835452266b01a
Jonathan Aponte <128061125+Jonathan-Aponte@users.noreply.github.com>
2023-10-16 20:09:52 +0200

    Update script.js
    V2

--- script.js
+++ script.js
@@ -21,9 +21,9 @@ function efectoHabilidades(){
     var distancia_skills = window.innerHeight - skills.getBoundingClientRect().top;
     if(distancia_skills >= 300){
         let habilidades = document.getElementsByClassName("progreso");
-        habilidades[0].classList.add("javascript");
-        habilidades[1].classList.add("htmlcss");
-        habilidades[2].classList.add("photoshop");
+        habilidades[0].classList.add("SQL (Structured Query Language)");
+        habilidades[1].classList.add("Programming languages (Python)");
+        habilidades[2].classList.add("Statistical analysis and modeling");
         habilidades[3].classList.add("wordpress");
         habilidades[4].classList.add("drupal");
         habilidades[5].classList.add("comunicacion");
